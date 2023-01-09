532f643395f84169068d84e4612a7613ffef34ac
1996Aurel <121833780+1996Aurel@users.noreply.github.com>
2023-01-10 08:31:42 +1100

    Update model_vae.py

--- python_folder/model_vae.py
+++ python_folder/model_vae.py
@@ -13,10 +13,10 @@ class Variational_Encoder(nn.Module):
         super(Variational_Encoder, self).__init__()
         self.linear1 = nn.Linear(10, 20)
         self.linear2 = nn.Linear(20, 40)
-        self.linear3 = nn.Linear(40,80)
-        self.linear4 = nn.Linear(80,40)
-        self.linear5 = nn.Linear(40,20)
-        self.linear6 = nn.Linear(20,5)
+        self.linear3 = nn.Linear(40, 80)
+        self.linear4 = nn.Linear(80, 40)
+        self.linear5 = nn.Linear(40, 20)
+        self.linear6 = nn.Linear(20, 5)
         self.linear7 = nn.Linear(5, latent_dims)
         self.linear8 = nn.Linear(5, latent_dims)
 
@@ -32,7 +32,7 @@ class Variational_Encoder(nn.Module):
         x = F.relu(self.linear6(x))
         mu =  self.linear7(x)
         sigma = torch.exp(self.linear8(x))
-        z = mu + sigma*self.N.sample(mu.shape)
+        z = mu + sigma * self.N.sample(mu.shape)
         self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
         return z
 
@@ -43,13 +43,13 @@ class Variational_Decoder(nn.Module):
         self.linear1 = nn.Linear(latent_dims, 5)
         self.linear2 = nn.Linear(5, 20)
         self.linear3 = nn.Linear(20, 40)
-        self.linear4 = nn.Linear(40,80)
-        self.linear5 = nn.Linear(80,40)
-        self.linear6 = nn.Linear(40,20)
-        self.linear7 = nn.Linear(20,10)     
+        self.linear4 = nn.Linear(40, 80)
+        self.linear5 = nn.Linear(80, 40)
+        self.linear6 = nn.Linear(40, 20)
+        self.linear7 = nn.Linear(20, 10)     
 
     def forward(self, z):
-        z = F.relu(self.linear1(z)) # relu(x) = max(0,x)
+        z = F.relu(self.linear1(z)) 
         z = F.relu(self.linear2(z))
         z = F.relu(self.linear3(z))
         z = F.relu(self.linear4(z))
